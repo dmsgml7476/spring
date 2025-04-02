bbfa4758502fb79006d0278d556a2cb309a50828
dmsgml7476 <dmsgml7476@gmail.com>
2025-04-02 18:57:51 +0900

    0402

--- talk/src/main/java/com/talk/control/MemberController.java
+++ talk/src/main/java/com/talk/control/MemberController.java
@@ -2,12 +2,28 @@ package com.talk.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.talk.DTO.MemberSignInDto;
+import com.talk.DTO.MemberSignUpDto;
 import com.talk.service.MemberService;
 
 @Controller
+@RequestMapping("/member")
 public class MemberController {
 	@Autowired
 	private MemberService memberService;
+	
+	@PostMapping("/signUp")
+	public String signUp (MemberSignUpDto memberSignUpDto, Model model) {
+		return null;
+	}
+	
+	@PostMapping("/signIn")
+	public String signIn (MemberSignInDto memberSignInDto, Model model) {
+		return null;
+	}
 
 }
